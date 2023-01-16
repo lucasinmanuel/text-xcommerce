8fdfd01602d2e9952c5a79b1b9d2887c775fa364
LucasInmanuel <lucasemanuel2077@gmail.com>
2023-01-16 11:32:42 -0300

    page variable fix

--- components/Main/Main.jsx
+++ components/Main/Main.jsx
@@ -107,9 +107,9 @@ function Main({ font }) {
 
     function nextPage(filter, direction) {
         let infoPage = JSON.parse(sessionStorage.getItem(filter))
-        let page = Number(infoPage.page);
-        let limit = Number(infoPage.limit);
-        let number_pages = Number(infoPage.number_pages);
+        let page = infoPage.page ? Number(infoPage.page) : ""
+        let limit = infoPage.limit ? Number(infoPage.limit) : ""
+        let number_pages = infoPage.number_pages ? Number(infoPage.number_pages) : ""
         if (direction === "next" && page + 1 <= number_pages) {
             getProducts(page + 1, limit, filter)
         } else if (direction === "previous" && page - 1 !== 0) {
